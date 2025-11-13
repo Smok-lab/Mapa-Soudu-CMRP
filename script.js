d9ba5925e802be79d4242d57dfff750c141f7faf
Smok-lab <smoksmokyt@gmail.com>
2025-11-13 22:20:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// List of deputies you can assign to positions
+const deputies = ["Cage", "Smith", "Johnson", "Brown"];
 
+// Store who is standing at which position
+let positions = {};
+
+function selectDeputy(positionId) {
+  const selected = prompt("Select a deputy: " + deputies.join(", "));
+  
+  if (selected) {
+    positions[positionId] = selected;
+    alert(`Position ${positionId} is now assigned to: ${selected}`);
+  }
+}
